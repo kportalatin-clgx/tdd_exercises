fe801c863a6b98126464005aebf149bb5378bd17
test <test@corelogic.com>
2019-05-16 15:57:31 -0500

    restore template

--- src/com/corelogic/k8port/conwaysgameoflife/ConwaysGameOfLife.java
+++ src/com/corelogic/k8port/conwaysgameoflife/ConwaysGameOfLife.java
@@ -1,29 +1,5 @@
 package com.corelogic.k8port.conwaysgameoflife;
 
 class ConwaysGameOfLife {
-    private int[][] gol;
 
-    void seed(int n) {
-        this.gol = new int[n][n];
-    }
-
-    void seed(int n, boolean initVal) {
-        this.gol = new int[n][n];
-        if (initVal == true) setValue(0, 0);
-    }
-
-    void setValue(int row, int col) {
-        this.gol[row][col] = 1;
-    }
-
-    void display() {
-        StringBuilder sb = new StringBuilder();
-        for (int[] row : this.gol) {
-            for (int col : row) {
-                sb.append(col + " ");
-            }
-            sb.append("\n");
-        }
-        System.out.println(sb.toString());
-    }
 }
--- src/com/corelogic/k8port/conwaysgameoflife/ConwaysGameOfLifeTest.java
+++ src/com/corelogic/k8port/conwaysgameoflife/ConwaysGameOfLifeTest.java
@@ -2,21 +2,6 @@ package com.corelogic.k8port.conwaysgameoflife;
 
 public class ConwaysGameOfLifeTest {
     public static void main(String[] args) {
-        test1();
-        initAllTrue();
-    }
-
-    private static void test1() {
-        ConwaysGameOfLife game = new ConwaysGameOfLife();
-        System.out.println("Test 1 assert non-empty 2dimensional array");
-        game.seed(1);
-        game.display();
-    }
-
-    private static void initAllTrue() {
-        ConwaysGameOfLife game = new ConwaysGameOfLife();
-        System.out.println("Init board to all true");
-        game.seed(1, true);
-        game.display();
+     
     }
 }
